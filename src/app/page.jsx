6efc7e5abfed8b2a95fd02827431fa86b140bc89
smRid <omorsk911@gmail.com>
2026-05-21 21:13:31 +0600

    implement LearningTips and CtaBand components in the homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,14 +2,18 @@ import HeroSection from "@/components/heroSection/HeroSection";
 import AvailableTutors from "@/components/AvailableTutors";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import HowItWorks from "@/components/HowItWorks";
+import LearningTips from "@/components/home/LearningTips";
+import CtaBand from "@/components/home/CtaBand";
 
 export default function HomePage() {
   return (
     <>
       <HeroSection />
       <AvailableTutors />
+      <LearningTips />
       <WhyChooseUs />
       <HowItWorks />
+      <CtaBand />
     </>
   );
 }
